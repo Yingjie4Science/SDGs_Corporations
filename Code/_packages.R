4cf1bdfde8e62fd7b184a459ef9034b480e56451
YingjieLiScience <yingjieli.edu@gmail.com>
2021-06-22 09:44:55 -0400

    pseudo code with Vero

---
+++ Code/_packages.R
@@ -0,0 +1,42 @@
+
+
+### manipulate data #####
+library(dplyr)
+library(plyr)
+library(tidyverse)
+library(splitstackshape)
+library(reshape2)   ## colsplit
+library(reshape)
+# library(xlsx)
+library(readxl)
+library(writexl)
+## shapefiles
+library(rnaturalearthdata)
+library(rnaturalearth)
+library(sf)
+library(scales)     ## percent
+library(classInt)   ## breaks
+
+### plot #####
+library(cowplot)
+library(ggplot2)
+library(ggpubr)
+library(RColorBrewer)
+## Color Brewer palette
+library(viridis)
+library(hrbrthemes)
+library(devtools)
+# install_github("jokergoo/ComplexHeatmap")
+# library(devtools)
+# library(utils)
+# remove.packages('ComplexHeatmap')
+
+library(ComplexHeatmap)
+# packageVersion('ComplexHeatmap')
+# R.Version()
+
+library(vegan) ## for evenness cal
+
+library(countrycode)
+library(knitr)           ## generate a fancy table
+library(kableExtra) ## generate a fancy table
