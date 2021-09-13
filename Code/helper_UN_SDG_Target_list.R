e5aa7d98b596df8fb9cdeaf842f3dfe44a20b307
YingjieLiScience <yingjieli.edu@gmail.com>
2021-09-13 16:24:03 -0400

    code all 17 SDGs and 169 targets

---
+++ Code/helper_UN_SDG_Target_list.R
@@ -0,0 +1,24 @@
+
+
+
+### UN Goal and Target list -----------------------------------------------------
+library(reshape2)
+
+goals_ls <- paste('SDG', seq(1,17), sep = ''); goals_ls
+goals_df <- data.frame(goal = goals_ls)
+
+
+ls_un <- read.csv('./Data/data_raw/_ls_un_goal_target.csv', stringsAsFactors = F) %>%
+  dplyr::filter(!is.na(GoalID)) %>%
+  tidyr::separate(Targets, c('target_id_un', 'target_desc_un'), sep = ' ', extra = 'merge', remove = T)
+str(ls_un)
+
+ls_un_id <- ls_un %>%
+  dplyr::select(GoalID, target_id_un) %>%
+  separate(target_id_un, c('target_id_un1', 'target_id_un2'), sep = '\\.', remove = F) %>%
+  dplyr::select(-target_id_un1) %>%
+  dplyr::mutate(goalname = paste0('SDG', GoalID),
+                goalname = factor(goalname, levels = goals_ls))
+str(ls_un_id)
+
+
