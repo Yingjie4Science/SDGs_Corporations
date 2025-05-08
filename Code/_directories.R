68749a9c413660a7c70cb09582d25f3838a4249a
Yingjie Li <yingjieli.edu@gmail.com>
2025-05-08 11:36:32 -0700

    minor edits

---
+++ Code/_directories.R
@@ -0,0 +1,16 @@
+
+
+
+## set work dir
+path <- rstudioapi::getSourceEditorContext()$path
+dir  <- dirname(rstudioapi::getSourceEditorContext()$path); dir
+dir_root <- dirname(dir)
+setwd(dir_root)
+getwd()
+
+
+
+dir.raw     <- paste0(dir_root, '/data/data_raw/')
+dir.tm      <- paste0(dir_root, '/data/data_tm/')
+dir.results <- paste0(dir_root, '/data/data_results/')
+dir.figures <- paste0(dir_root, '/figures/')
