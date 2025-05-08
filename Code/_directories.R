2de1c83f821388a9f9cfa172c552570a621cbc28
Yingjie Li <yingjieli.edu@gmail.com>
2025-05-08 14:59:21 -0700

    tidy path names

--- Code/_directories.R
+++ Code/_directories.R
@@ -11,6 +11,8 @@ getwd()
 
 
 dir.raw     <- paste0(dir_root, '/data/data_raw/')
+
 dir.tm      <- paste0(dir_root, '/data/data_tm/')
+dir.reports <- paste0(dir.tm,'annual_reports/') ## PDFs are stored here to save space
 dir.results <- paste0(dir_root, '/data/data_results/')
 dir.figures <- paste0(dir_root, '/figures/')
