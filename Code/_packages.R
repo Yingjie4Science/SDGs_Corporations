57b912db9c64048d5180abc75633db77cb95c215
Yingjie Li <yingjieli.edu@gmail.com>
2025-08-23 11:32:40 -0700

    minor edits

--- Code/_packages.R
+++ Code/_packages.R
@@ -1,10 +1,12 @@
 
 
 ### manipulate data #####
+library(here)
 library(dplyr)
 library(tidyr)
 library(splitstackshape)
 library(stringr)
+library(scales)
 library(reshape2)   ## colsplit
 library(reshape)
 library(readr)
@@ -15,4 +17,8 @@ library(RColorBrewer)
 library(ggplot2)
 library(ggpubr)
 library(cowplot)
+
+Sys.setlocale("LC_ALL", "English")
+ymd <- format(Sys.time(), "%Y%m%d"); ymd
+
 
